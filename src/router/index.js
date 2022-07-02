d89f906111d6332f85c5132e71a0190806d4dce0
Shubham Ravani <shubhamravani67890@gmail.com>
2022-07-02 23:59:52 +0530

    Travel App

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import store from '@/store'
 
 const routes = [
   {
@@ -8,18 +9,83 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/destination/:slug',
+    name: 'DestinationDetails',
+    props: true,
+    component: () =>
+      import(/* webpackChunkName: "DestinationDetails" */ '../views/DestinationDetails.vue'),
+    children: [
+      {
+        path: ':experienceSlug',
+        name: 'experienceDetails',
+        props: true,
+        component: () =>
+          import(/* webpackChunkName: "ExperienceDetails" */ '../views/ExperienceDetails.vue')
+      }
+    ],
+    beforeEnter: (to, from, next) => {
+      const exists = store.destinations.find(
+        destination => destination.slug === to.params.slug
+      )
+      if (exists) {
+        next()
+      } else {
+        next({ name: 'notFound' })
+      }
+    }
+  },
+  {
+    path: '/UserView',
+    name: 'UserView',
+    component: () =>
+      import(/* webpackChunkName: "User" */ '../views/UserView.vue'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/LoginView',
+    name: 'LoginView',
+    component: () =>
+      import(/* webpackChunkName: "Login" */ '../views/LoginView.vue')
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
+  linkExactActiveClass: 'travel-active-class',
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      const position = {}
+      if (to.hash) {
+        position.selector = to.hash
+        if (to.hash === '#experience') {
+          position.offset = { y: 140 }
+        }
+        if (document.querySelector(to.hash)) {
+          return position
+        }
+
+        return false
+      }
+    }
+  },
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.user) {
+      next({
+        name: 'LoginView',
+        query: { redirect: to.fullPath }
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
